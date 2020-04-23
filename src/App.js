c6823ddbc0664d20c86ed76bcc558f99b5f90446
Fernando <fernando_0970@outlook.es>
2020-04-23 01:14:39 -0500

    Extensions added to the project: prettier, scripts, ESlint setup and config and .gitnore file

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
-const Pet = ([name, animal, bread]) => {
+const Pet = ({ name, animal, bread }) => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, name),
     React.createElement("h1", {}, animal),
     React.createElement("h1", {}, bread),
   ]);
-}
+};
 
 const App = () => {
-  return React.createElement("div", { id: "something-important" }, [ 
-    React.createElement("h1", {}, "Adopt Me!"), 
-      React.createElement(Pet, {
-        name: "Luna",
-        animal: "Dog", 
-        bread: "Havanese"
-      }),
-      React.createElement(Pet, {
-        name: "Hima", 
-        animal: "Bird", 
-        bread: "Cocktail"
-      }),
-      React.createElement(Pet, {
-        name: "Don Cat",
-        animal: "Cat", 
-        bread: "Mixed"
-      })
+  return React.createElement("div", { id: "something-important" }, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      bread: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Hima",
+      animal: "Bird",
+      bread: "Cocktail",
+    }),
+    React.createElement(Pet, {
+      name: "Don Cat",
+      animal: "Cat",
+      bread: "Mixed",
+    }),
   ]);
 };
 
-ReactDOM.render(
-  React.createElement(App), document.getElementById("root")
-);
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
