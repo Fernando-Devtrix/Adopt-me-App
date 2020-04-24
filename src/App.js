2863d310fdc635360aeaac32309023f2a4882732
Fernando <fernando_0970@outlook.es>
2020-04-23 23:27:56 -0500

    Hooks: creating a Search Compoonent, setting state, configuring ESlint for hooks, call Pet API, breed dropdown, custom hooks

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  // return React.createElement("div", { id: "something-important" }, [
-  //   React.createElement("h1", {}, "Adopt Me!"),
-  //   React.createElement(Pet, {
-  //     name: "Luna",
-  //     animal: "Dog",
-  //     bread: "Havanese",
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Hima",
-  //     animal: "Bird",
-  //     bread: "Cocktail",
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Don Cat",
-  //     animal: "Cat",
-  //     bread: "Mixed",
-  //   }),
-  // ]);
   return (
     <div>
       <h1 id="something-important">Adopt me</h1>
-      <Pet name="Luna" animal="Dog" bread="Havanese" />
-      <Pet name="Hima" animal="Bird" bread="Cocktail" />
-      <Pet name="Don Cat" animal="Cat" bread="Mixed" />
+      <SearchParams />
     </div>
   );
 };
